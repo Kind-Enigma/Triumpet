cc1740c4f9f23bc46769cc0909316fa59f84ca1b
Wayne Muller <github@wainage.com>
2015-02-03 14:17:25 -0800

    Changed Mongo connection string connecting to our MongoLab db.

--- server/app.js
+++ server/app.js
@@ -5,10 +5,18 @@ var app = express();
 
 //Declares Database path and sets default to localhost.
 //Please remember to set process.env.dbPath on your deployment environment
-var dbPath         = process.env.dbPath || 'mongodb://localhost/triumpet';
+var dbPath = process.env.dbPath || 'mongodb://ds045097.mongolab.com:45097/triumpet';
 
 //connect to mongo
-mongoose.connect(dbPath);
+mongoose.connect(dbPath, {
+  user: "admin",
+  pass: "triumpet"
+});
+
+mongoose.connection.on('error', function(error){
+  console.log("Can't connect to mongoDB.");
+  console.log(error);
+});
 
 //configure server with all middleware and routing
 require('./config/router.js')(app, express);
