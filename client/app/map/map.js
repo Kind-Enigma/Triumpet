763c9fc88dfc6cd6d46eba3178bf0b2ea5cb1c14
Jason Yang <jasonyang@Jasons-MacBook-Air.local>
2015-02-05 13:38:14 -0800

    draw a line between user and target item, and update path when user is moving

--- client/app/map/map.js
+++ client/app/map/map.js
@@ -42,7 +42,55 @@ angular.module('tp.map',[])
         .attr('width', 30 * scope.scale + 'px' ) // [Warning]: 30 is a hardcoded width for this particular map, this is has to be changed to accomodate different maps
         .attr('height', 20 * scope.scale + 'px') // [Warning]: 20 is a hardcoded height for this particular map, this is has to be changed to accomodate different maps
         .on('click', addUserToMap); // [Note]: Uses d3 to handle this event, not angular
- 
+    //===========================\\
+    // var objectifyData = function(array){
+    //   var data = [];
+    //   for (var i  = 0; i < array.length; i++){
+    //     data.push({"x": array[i][0], "y": array[i][1]})
+    //   }
+
+    //   return data;
+    // };
+
+
+
+    var data = [[Number($('.user').attr('cx')), Number($('.user').attr('cy'))],
+                [Number($('.item').attr('cx')), Number($('.item').attr('cy'))]];
+
+    var drawPath = function(data){
+      d3.select("path").remove()
+      var lineFunction = d3.svg.line()
+          .x(function(d) { return d[0] })
+          .y(function(d) { return d[1] })
+          .interpolate("linear");
+
+      d3.select('svg').append("path")
+                      .attr("d", lineFunction(data))
+                      .attr("stroke", "blue")
+                      .attr("stroke-width", 5)
+                      .attr("fill", "none");
+    }
+
+
+    d3.select('svg')
+      .append('text')
+      .attr('fill', 'white')
+      .attr('y', 3 * scope.scale + 'px')
+      .attr('x', 42 * scope.scale + 'px')
+      .style('cursor', 'hand')
+      .text('Calculate Route')
+      .on('click', function() {
+          d3.select('svg > #route')
+          // .transition()
+          // .duration(5000)
+          .attrTween('d', drawPath(data))
+    });
+
+
+
+
+    //\\===========================//\\
+
     // Defines the drag behavior.
     var drag = d3.behavior.drag()
       .on("drag", dragMove);
@@ -50,6 +98,9 @@ angular.module('tp.map',[])
     // Used in dragMove, below.
     function updateUserLoc(userLoc){
       scope.userLoc = userLoc;
+      data = [[Number($('.user').attr('cx')), Number($('.user').attr('cy'))],
+              [Number($('.item').attr('cx')), Number($('.item').attr('cy'))]];
+      drawPath(data);
     };
 
     // Callback to be invoked on the 'drag' event.
@@ -58,6 +109,18 @@ angular.module('tp.map',[])
       var y = d3.event.y;
       d3.select(this).attr('cx',x).attr('cy', y);
       updateUserLoc({x:x, y:y});
+
+      // var data = [{"x": x + 20, "y": y + 20}, {"x":20, "y":40}]
+
+      // var line = d3.svg.line()
+      //     .x(function(d) { return d[0]; })
+      //     .y(function(d) { return d[1]; });
+
+      // d3.select("svg").append("path")
+      //   .attr("class", "line")
+      //   .attr("d", line(data));
+
+      // lineFunction(data);
     };
 
     // Adds user circle to the map. This is blue in the demo.
