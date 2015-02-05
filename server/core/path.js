7c5363299ec338c727eb837b547e3917bde76492
Wayne Muller <github@wainage.com>
2015-02-05 12:17:13 -0800

    Path.distance function added
    Takes array of path points and returns distance in units travelled

---
+++ server/core/path.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+var distance = function(path) {
+
+  return path.reduce(function(previous, item){
+    if (previous[1].length === 0) {
+      previous[1] = item;
+    } else {
+      var x = item[0]-previous[1][0];
+      var y = item[1]-previous[1][1];
+      var distance = Math.sqrt((x*x)+(y*y));
+      previous[0] += distance;
+      previous[1] = item;
+    }
+    return previous;
+  },[0,[]]);
+
+};
