829e2d7eee543353f807e4601a47205a6345aefd
Wayne Muller <github@wainage.com>
2015-02-05 13:37:12 -0800

    Added createArea to Grid object

--- server/core/grid.js
+++ server/core/grid.js
@@ -3,13 +3,14 @@
 // ------------------------------------------------
 // Grid object
 // ------------------------------------------------
-// Takes a W x H area and devides it into gridSize
+// Takes a W x H area and divides it into gridSize
 // grids.
 // Grid can be passed to pathfinding algo's to solve shortest
-// path problems
+// path problems.
+// Defaults to value 1 == unwalkable in pathfinding
 
 
-var CreateGrid = function(width, height, gridSize, value) {
+var Grid = function(width, height, gridSize, value) {
   this.grid = [];
   this.gridSize = gridSize;
 
@@ -22,4 +23,21 @@ var CreateGrid = function(width, height, gridSize, value) {
     }
   }
 
+};
+
+// -------------------------------
+// createArea
+// -------------------------------
+// Creates an area defined by (x, y, toX, toY)
+// as absolute co-ordinates and sets value. Defaults
+// to 0 which will create areas that can be walked
+
+Grid.prototype.createArea = function(x1, y1, x2, y2, value) {
+  value = value || 0;
+
+  for (var j = Math.floor(y1 / this.gridSize); j < Math.floor(y2 / this.gridSize); j++) {
+    for (var i = Math.floor(x1 / this.gridSize); i < Math.floor(x2 / this.gridSize); i++) {
+      this.grid[i][j] = value;
+    }
+  }
 };
