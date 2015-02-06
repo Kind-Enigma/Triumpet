2344ec9c724bd6f07dec5ef752ec8771e987dc98
Jason Yang <jasonyang@Jasons-MacBook-Air.local>
2015-02-06 12:24:58 -0800

    change the color of item to yellow after the user have reached the item once

--- client/app/factories/factories.js
+++ client/app/factories/factories.js
@@ -117,10 +117,11 @@ angular.module('tp.factories',[])
 
 	// Draws items on svg specified as one of the inputs.
   item.drawItems = function(items, scale, svg, clear){
-    item.storage.push(items)
-    var coordinatesArray = [scale * items[0].coordinates[0].x, scale * items[0].coordinates[0].y]
-    item.coordinatesStorage.push(coordinatesArray)
-    console.log(item.coordinatesStorage)
+
+    var coordinatesArray = [scale * items[0].coordinates[0].x, scale * items[0].coordinates[0].y];
+    item.coordinatesStorage.push(coordinatesArray);
+    item.storage.push(items);
+    // console.log(item.coordinatesStorage)
 
 
 
@@ -145,7 +146,6 @@ angular.module('tp.factories',[])
        .attr('fill','red')
 
   };
-
 	return item;
 })
 
